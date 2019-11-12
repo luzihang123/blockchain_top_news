ffd0be949b4ba210cf27a129f2dc2b1252c7c424
luzihang123 <526154064@qq.com>
2019-11-12 16:51:35 +0800

    完成:文章列表

--- BlockchainTopNews/home/admin.py
+++ BlockchainTopNews/home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from home.models import Category, Tag, Article
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Article)
